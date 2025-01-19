56c3a4a894ea2148214fbf64d5fa1ae2eeab93bf
FouadAllaoui <fouadallaoui70@gmail.com>
2025-01-19 16:43:54 +0100

    test(change): change the indexjs to test the pipelines

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h1 className={styles.title}>
+          Welcome to Nostrad
+        </h1>
+
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
